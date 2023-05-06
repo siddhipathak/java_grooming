360f9e30da1b2c587c7750965c42eae841314ba3
Siddhi Pathak <69953224+siddhipathak@users.noreply.github.com>
2023-05-06 12:01:34 +0530

    Add files via upload

--- assignment3/q1/src/main/java/com/example/q1/Q1Application.java
+++ assignment3/q1/src/main/java/com/example/q1/Q1Application.java
@@ -1,8 +1,9 @@
 package com.example.q1;
 
 import java.util.List;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.example.q1.enums.Category;
 import com.example.q1.model.Product;
 import com.example.q1.utility.Utility;
@@ -21,15 +22,23 @@ public class Q1Application {
 					new Product("6", "Sony", false, Category.TV),
 					new Product("7", "Sony", true, Category.REFRIGERATOR),
 					new Product("8", "Samsung", false, Category.REFRIGERATOR));
-			Category category = Category.MOBILE;
+		
+			Scanner s = new Scanner(System.in);
+			System.out.println("Enter the category you want product of:");
+			String inputCategory = s.nextLine();
+			Category category = Utility.findByName(inputCategory);
+			
 			List<List<Product>> result = Utility.findItem(products, category);
-
 			System.out.println("Product with warranty are:");
 			System.out.println(result.get(0));
 			System.out.println("Product without warranty are:");
 			System.out.println(result.get(1));
+
+			s.close();
+			
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
+			System.exit(0);
 		}
 	}
 
